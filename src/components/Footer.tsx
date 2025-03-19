c58da9ad20af0cdf54f2c429f505e006d126b8d3
Parvashukla26 <parvashukla26@gmail.com>
2025-03-19 10:50:08 +0530

    Fixed deployment issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,13 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "../data";
 import MagicButton from "./MagicButton";
-
+import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
+        <Image
           src="/footer-grid.svg"
           alt="grid"
           className="w-full h-full opacity-50 "
@@ -21,8 +21,8 @@ const Footer = () => {
           digital world?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Let’s bring your vision to life with creativity, strategy, and
-          innovation. <br /> Whether you're starting fresh or leveling up, I’m
+          Let&apos;s bring your vision to life with creativity, strategy, and
+          innovation. <br /> Whether you&apos;re starting fresh or leveling up, I&apos;m
           here to craft something extraordinary for you.
         </p>
         <a href="mailto:contact@jsmastery.pro">
@@ -35,7 +35,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Parva Shukla's Folio
+          Parva Shukla&apos;s Folio
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
@@ -47,7 +47,7 @@ const Footer = () => {
               rel="noopener noreferrer"
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={info.img} alt="icons" width={20} height={20} />
+              <Image src={info.img} alt="icons" width={20} height={20} />
             </a>
           ))}
         </div>
